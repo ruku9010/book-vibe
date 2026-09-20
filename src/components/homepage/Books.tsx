bd9272a36e2b75d50eeab9597473c3db2a85761b
ross9010 <rosstaylorpronty99@gmail.com>
2026-09-20 17:43:38 +0600

    Typescript added and map booksData

--- src/components/homepage/Books.tsx
+++ src/components/homepage/Books.tsx
@@ -1,7 +1,9 @@
+import { IBook } from "@/app/types/books.types";
+import BookCard from "../shared/BookCard";
 
 
 const getBooks = async()=>{
-    const response = await fetch('/public/booksData.json');
+    const response = await fetch('http://localhost:3000/booksData.json');
     const data = await response.json();
     return data
 }
@@ -13,6 +15,13 @@ const Books = async() => {
     return (
         <div className='container mx-auto'>
             Books
+            <div className="grid grid-cols-4 gap-3">
+                {
+                    booksData.map((book: IBook)=> {
+                        return <BookCard key={book.bookId} book={book}/>
+                    })
+                }
+            </div>
         </div>
     );
 };
---
+++ src/components/shared/BookCard.tsx
@@ -0,0 +1,28 @@
+import { IBook } from '@/app/types/books.types';
+import Image from 'next/image';
+import React from 'react';
+import { FaStar } from 'react-icons/fa';
+
+interface IBookCardProps {
+    book: IBook
+}
+
+const BookCard = ({book}: IBookCardProps) => {
+    return (
+        <div className='flex flex-col gap-3 items-center w-full'>
+            <Image src={book.image} alt={book.bookName} width={200} height={160}/>
+            <div className='flex gap-5'>
+                <span>{book.tags[0]}</span>
+            <span>{book.tags[1]}</span>
+            </div>
+            <h2>{book.bookName}</h2>
+            <p>By: {book.author}</p>
+            <div className='flex justify-around'>
+                <span>{book.category}</span>
+                <span className='flex gap-1 items-center'>{book.rating} <FaStar /></span>
+            </div>
+        </div>
+    );
+};
+
+export default BookCard;
