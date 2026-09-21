0624000616a9e6b5986c3921aa8a0ae3e38824b6
ruku901 <rukunujjamans@gmail.com>
2026-09-21 22:46:38 +0600

    Type added

--- src/context/BooksProvider.tsx
+++ src/context/BooksProvider.tsx
@@ -1,14 +1,27 @@
 "use client"
 
-import { createContext, ReactNode, useState } from "react";
+import { IBook } from "@/app/types/books.types";
+import { createContext, Dispatch, ReactNode, SetStateAction, useState } from "react";
 
-export const BooksContext = createContext({})
+interface IBooksContext {
+    readBooks: IBook[];
+    setReadBooks: Dispatch<SetStateAction<IBook[]>>;
+    wishlist: IBook[];
+    setWishlist: Dispatch<SetStateAction<IBook[]>>;
+}
+
+export const BooksContext = createContext<IBooksContext>({
+    readBooks: [],
+    setReadBooks: () => {},
+    wishlist: [],
+    setWishlist: () => {},
+})
 
 
 const BooksProvider = ({children}: {children: ReactNode}) => {
 
-    const [readBooks, setReadBooks] = useState([])
-    const [wishlist, setWishlist] = useState([])
+    const [readBooks, setReadBooks] = useState<IBook[]>([])
+    const [wishlist, setWishlist] = useState<IBook[]>([])
 
     const sharedData = {
         readBooks,
