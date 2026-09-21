4c26e228284c50baa1a4dc70edf9fb31e2a34517
ruku901 <rukunujjamans@gmail.com>
2026-09-21 22:35:01 +0600

    env added and try-catch added where needed

--- src/components/homepage/Books.tsx
+++ src/components/homepage/Books.tsx
@@ -3,9 +3,15 @@ import BookCard from "../shared/BookCard";
 
 
 const getBooks = async()=>{
-    const response = await fetch('http://localhost:3000/booksData.json');
-    const data = await response.json();
-    return data
+    try{
+        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`);
+        const data = await response.json();
+        return data;
+    
+    }catch(error){
+        console.error("Error fetching books data:", error)
+        return [];
+    }
 }
 
 const Books = async() => {
